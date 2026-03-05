e91f7d3e56e169dcb445e85e461d5c1935da2c1b
kamilatresnickova <kamila.cichrova@icloud.com>
2026-03-05 20:26:30 +0100

    Update 03_exploratory_analysis.sql

--- 03_exploratory_analysis.sql
+++ 03_exploratory_analysis.sql
@@ -140,4 +140,123 @@ GROUP BY hotel;
 -- 2. CUSTOMER SEGMENTATION & BEHAVIOR ANALYSIS
 -- =============================================================================
 
--- 2.1. Geographic segmentation
+-- 2.1. Geographic segmentation - Top 5 countries by revenue share per hotel
+
+-- STEP 1: Revenue calculation per country and hotel
+WITH HotelRevenue AS (
+    SELECT 
+        hotel,
+        country,
+        ROUND(SUM(adr * (stays_in_weekend_nights + stays_in_week_nights)), 0) AS total_revenue,
+        COUNT(*) AS booking_count
+    FROM hotel_bookings
+    WHERE is_canceled = 0
+    GROUP BY hotel, country
+)
+-- STEP 2: Calculate revenue share and rank countries
+SELECT * FROM (
+    SELECT 
+        hotel,
+        country,
+        total_revenue,
+        booking_count,
+        ROUND(total_revenue::numeric / SUM(total_revenue) OVER(PARTITION BY hotel) * 100, 2) AS revenue_share_pct,
+        RANK() OVER(PARTITION BY hotel ORDER BY total_revenue DESC) as revenue_rank
+    FROM HotelRevenue
+) sub
+WHERE revenue_rank <= 5
+ORDER BY hotel, revenue_rank;
+
+--2.2. Customer Segmentation - Families vs. Guests without Children
+
+SELECT 
+    hotel,
+    CASE 
+        WHEN children > 0 OR babies > 0 THEN 'Family'
+        ELSE 'No Children'
+    END AS guest_segment,
+    COUNT(*) AS total_bookings,
+    ROUND(AVG(adr), 2) AS avg_daily_rate,
+    ROUND(AVG(stays_in_weekend_nights + stays_in_week_nights), 2) AS avg_length_of_stay
+FROM hotel_bookings
+WHERE is_canceled = 0
+GROUP BY hotel, guest_segment
+ORDER BY hotel, guest_segment;
+
+
+-- 2.3. Room Upgrades Analysis - percentage of guests receiving a different room type than reserved (assigned vs. reserved)
+SELECT 
+    hotel,
+    CASE 
+        WHEN reserved_room_type = assigned_room_type THEN 'Match'
+        ELSE 'Upgrade/Change'
+    END AS room_assignment,
+    COUNT(*) AS total_bookings,
+    ROUND(COUNT(*)::numeric / SUM(COUNT(*)) OVER(PARTITION BY hotel) * 100, 2) AS share_pct
+FROM hotel_bookings
+WHERE is_canceled = 0
+GROUP BY hotel, room_assignment
+ORDER BY hotel, share_pct DESC;
+
+-- 2.4. Revenue and ADR by Market Segment 
+SELECT 
+    hotel,
+    market_segment,
+    COUNT(*) AS total_bookings,
+    ROUND(AVG(adr), 2) AS avg_adr,
+    ROUND(AVG(lead_time), 0) AS avg_lead_time_days
+FROM hotel_bookings
+WHERE is_canceled = 0
+GROUP BY hotel, market_segment
+ORDER BY hotel, total_bookings DESC
+
+-- 2.5. Revenue and ADR by Repeated Guests
+SELECT 
+    hotel,
+    is_repeated_guest,
+    COUNT(*) AS total_bookings,
+    ROUND(AVG(adr), 2) AS avg_adr,
+    ROUND(AVG(previous_cancellations), 2) AS avg_prev_cancellations
+FROM hotel_bookings
+WHERE is_canceled = 0
+GROUP BY hotel, is_repeated_guest;
+
+-- 2.6. Meal Plan Preferences 
+SELECT 
+    hotel,
+    meal,
+    COUNT(*) AS count_bookings,
+    ROUND(COUNT(*)::numeric / SUM(COUNT(*)) OVER(PARTITION BY hotel) * 100, 2) AS share_pct
+FROM hotel_bookings
+WHERE is_canceled = 0
+GROUP BY hotel, meal
+ORDER BY hotel, share_pct DESC;
+
+
+/* FINDINGS: 
+- CITY HOTEL:
+   - Revenue share: Financial Leader FRA with 17.57% revenue share, surpassing PRT despite having fewer bookings (7,069 vs 10,793).
+   - Top 5 countries by revenue share: FRA, PRT, DEU, GBR, and ESP.
+   - Guest demographics: Families pay significantly more (146.76) than guests without children (102.65). That is a 43% price premium.
+   - Stay Duration: Families stay slightly longer (3.10 nights vs 2.90 nights).
+   - Room Upgrades: 85.55% of guests received the exact room type they booked, 14.45% of guests experienced a change.
+   - Market Segment: Volume Leader: Online TA (24,192 bookings), Profitability Leader: Direct (119.83 ADR), Planning Leader: Groups & Offline TA (122 days),Planning Loser: Aviation (4 days),Lowest Yield: Corporate (82.07 ADR).
+   - Repeated Guests: Small minority of the business (1,538 bookings, approx. 3.3% of the total).
+   - Pricing strategy: Repeated guests pay a much lower ADR (63.79 €) compared to new guests (107.49 €), a decrease of 40%.
+   - Cancellation Behavior: Repeated guests have quite high history of previous cancellations, averaging 0.63 per guest.
+   - Meal Plan Preferences: BB (Bed & Breakfast) is the primary choice at 77.19%, secondary choice is SC (Self Catering) with 14.17% share, FB (Full Board) is practically non-existent (0.02%).
+- RESORT HOTEL:
+   - Revenue share: financial Leader PRT with 28.57% revenue share, but GBR is a very close second with 24.24% share.
+   - Efficiency Gap: GBR generates nearly the same revenue as PRT but with only 5,921 bookings compared to PRT's 10,184. 
+   - Top 5 countries by revenue share: PRT, GBR, ESP, IRL, and FRA.
+   - Guest demographics: Families have significantly higher ADR of 151.90, while guests without children pay only 84.97. 
+     Families pay nearly DOUBLE (+78%).
+   - Stay Duration: Families represent the longest stays in the entire dataset (4.41 nights on average, comapred to 4.12 nights for guests without children).
+   - Room Upgrades: 74.65% of guests received the exact room type they booked, 25.35% of guests experienced a change. 
+   - Market Segment: Volume Leader: Online TA (11,481 bookings), Profitability Leader: Direct (108.99 ADR), Planning Leader: Groups (162 days), Planning Loser: Corporate (16 days), Lowest Yield: Corporate (49.61 ADR)
+   - Repeated Guests: Slightly higher loyalty rate with 1,666 bookings of repeated guests (approx. 5.8% of the total).
+   - Pricing Strategy: Repeated guests pay a lower ADR (64.02 €) compared to first-time visitors (92.46 €).
+   - Cancellation Behavior: Much cleaner cancelation history (0.07) compared to their counterparts in the City Hotel.
+   - Meal Plan Preferences: BB is the primary choice at 76.61%. Resort Specifics: HB (Half Board) is significantly higher here (19.01%) compared to the City Hotel. While still a niche, FB is much more relevant (1.08%).
+   
+   */
